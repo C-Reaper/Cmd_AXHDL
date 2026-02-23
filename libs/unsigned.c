1f5be02e19a87d09dba9d44f0ff5fe71a03c0a71
codeleaded <gnu.c.reaper@gmail.com>
2026-02-24 00:54:01 +0100

    updated

---
+++ libs/unsigned.c
@@ -0,0 +1,80 @@
+#include "/home/codeleaded/System/Static/Library/AlxExternFunctions.h"
+#include "/home/codeleaded/System/Static/Library/AXHDL.h"
+
+Token Unsigned_Unsigned_Handler_Ass(AXHDL* hdl,Token* op,Vector* args){
+    Token* a = (Token*)Vector_Get(args,0);
+    Token* b = (Token*)Vector_Get(args,1);
+
+    printf("[Unsigned]: ASS: %s = %s\n",a->str,b->str);
+
+    if(b->tt==TOKEN_NUMBER){
+        
+    }else if(b->tt==TOKEN_STRING){
+        
+    }else{
+        AXHDL_ErrorHandler(hdl,"Ass: Error -> %s has no pointer type!",b->str);
+        return Token_Null();
+    }
+    return Token_Cpy(a);
+}
+/*
+Token Unsigned_Unsigned_Handler_Add(AXHDL* hdl,Token* op,Vector* args){
+    Token* a = (Token*)Vector_Get(args,0);
+    Token* b = (Token*)Vector_Get(args,1);
+    
+    int realsize_a = AXHDL_TypeDrefPtrSize(hdl,a);
+    CStr imm = Number_Get(realsize_a);
+    Token ret = AXHDL_ExecuteP(hdl,a,b,op,"add","ADD","imul",imm,AXHDL_Function_Add,AXHDL_Function_Mul);
+    CStr_Free(&imm);
+    return ret;
+}
+Token Unsigned_Unsigned_Handler_Sub(AXHDL* hdl,Token* op,Vector* args){
+    Token* a = (Token*)Vector_Get(args,0);
+    Token* b = (Token*)Vector_Get(args,1);
+    
+    int realsize_a = AXHDL_TypeDrefPtrSize(hdl,a);
+    CStr imm = Number_Get(realsize_a);
+    Token ret = AXHDL_ExecuteP(hdl,a,b,op,"sub","SUB","imul",imm,AXHDL_Function_Sub,AXHDL_Function_Mul);
+    CStr_Free(&imm);
+    return ret;
+}
+Token Unsigned_Unsigned_Handler_Equ(AXHDL* hdl,Token* op,Vector* args){
+    Token* a = (Token*)Vector_Get(args,0);
+    Token* b = (Token*)Vector_Get(args,1);
+    //return AXHDL_ExecuteCmp(hdl,a,b,op,"sete","EQU",AXHDL_Function_Equ);
+    return AXHDL_ExecuteJmp(hdl,a,b,op,"jz","EQU",AXHDL_Function_Equ);
+}
+*/
+Token Unsigned_Null_Handler_Cast(AXHDL* hdl,Token* op,Vector* args){
+    Token* a = (Token*)Vector_Get(args,0);
+    return Token_By(TOKEN_CONSTSTRING_DOUBLE,"NONE");
+}
+Token Unsigned_Handler_Cast(AXHDL* hdl,Token* op,Vector* args){
+    Token* a = (Token*)Vector_Get(args,0);
+
+    if(op->str==NULL) return Unsigned_Null_Handler_Cast(hdl,op,args);
+    return Token_Null();
+}
+
+void Ex_Packer(ExternFunctionMap* Extern_Functions,Vector* funcs,AXHDL* hdl){//Vector<CStr>
+    TypeMap_PushContained(&hdl->types,funcs,
+        Type_Make(AXHDL_TYPE_UNSIGNED,sizeof(AXHDL_Signal),OperatorInterationMap_Make((OperatorInterater[]){
+            OperatorInterater_Make((CStr[]){ NULL },OperatorDefineMap_Make((OperatorDefiner[]){
+                OperatorDefiner_New(TOKEN_CAST,(Token(*)(void*,Token*,Vector*))Unsigned_Handler_Cast),
+                //OperatorDefiner_New(TOKEN_AXHDL_SIZE,(Token(*)(void*,Token*,Vector*))Unsigned_Handler_Size),
+                //OperatorDefiner_New(TOKEN_DESTROY,NULL),
+                OPERATORDEFINER_END
+            })),
+            OperatorInterater_Make((CStr[]){ AXHDL_TYPE_UNSIGNED,NULL },OperatorDefineMap_Make((OperatorDefiner[]){
+                OperatorDefiner_New(AXHDL_TOKEN_ASS,(Token(*)(void*,Token*,Vector*))Unsigned_Unsigned_Handler_Ass),
+                //OperatorDefiner_New(AXHDL_TOKEN_ADD,(Token(*)(void*,Token*,Vector*))Unsigned_Unsigned_Handler_Add),
+                //OperatorDefiner_New(AXHDL_TOKEN_SUB,(Token(*)(void*,Token*,Vector*))Unsigned_Unsigned_Handler_Sub),
+                //OperatorDefiner_New(AXHDL_TOKEN_EQU,(Token(*)(void*,Token*,Vector*))Unsigned_Unsigned_Handler_Equ),
+                OPERATORDEFINER_END
+            })),
+            OPERATORINTERATER_END
+        }),
+        (void(*)(Variable*))AXHDL_Signal_Free,
+        (void(*)(Variable*,Variable*))AXHDL_Signal_Cpy)
+    );
+}
--- src/Main.c
+++ src/Main.c
@@ -1,14 +1,32 @@
 #include "/home/codeleaded/System/Static/Library/AXHDL.h"
 
+
+void Fn_Runner(AXHDL* hdl,AXHDL_U64 step){
+    AXHDL_Signal in = AXHDL_Signal_New("u1","a",(AXHDL_U8[]){ 0x01U },1U,AXHDL_SIGNAL_DIR_OUT);
+    AXHDL_Signal out = AXHDL_Signal_New("u1","b",(AXHDL_U8[]){ 0x00U },1U,AXHDL_SIGNAL_DIR_IN);
+    
+    AXHDL_Run_Arch(
+        hdl,
+        "Main",
+        &in,
+        &out
+    );
+    printf("[Sim]: Out: %x\n",((AXHDL_U8*)out.data)[0]);
+
+    AXHDL_Signal_Free(&out);
+    AXHDL_Signal_Free(&in);
+}
+
 int main(){
-    AXHDL hdl = AXHDL_New();
-    AXHDL_Load(&hdl,"./code/Main.axhdl");
+    AXHDL hdl = AXHDL_New("./libs");
+    
+    AXHDL_Push_Signal(&hdl,AXHDL_Signal_New("u1","clk",(AXHDL_U8[]){ 0x01U },1U,AXHDL_SIGNAL_DIR_OUT));
+    AXHDL_Push_Signal(&hdl,AXHDL_Signal_New("u1","reset",(AXHDL_U8[]){ 0x01U },1U,AXHDL_SIGNAL_DIR_OUT));
+    
+    AXHDL_Script(&hdl,"./code/Main.axhdl");
+    AXHDL_Run_Sim(&hdl,100ULL,Fn_Runner);
 
-    AXHDL_U64 in[] = { 0x1 };
-    AXHDL_U64 out[] = { 0x0 };
-    AXHDL_Run_Arch(&hdl,"Main",(void*)in,(void*)out);
-    printf("%x -> %x\n",in,out);
-
+    AXHDL_Print(&hdl);
     AXHDL_Free(&hdl);
     return 0;
 }
