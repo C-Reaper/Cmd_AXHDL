a0bd266f0cbe9283fde9ba30d16fe0e68834b36a
codeleaded <gnu.c.reaper@gmail.com>
2026-03-12 21:04:27 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,32 +1,37 @@
 #include "/home/codeleaded/System/Static/Library/AXHDL.h"
-
-
-void Fn_Runner(AXHDL* hdl,AXHDL_U64 step){
-    AXHDL_Signal in = AXHDL_Signal_New("u1","a",(AXHDL_U8[]){ 0x01U },1U,AXHDL_SIGNAL_DIR_OUT);
-    AXHDL_Signal out = AXHDL_Signal_New("u1","b",(AXHDL_U8[]){ 0x00U },1U,AXHDL_SIGNAL_DIR_IN);
-    
-    AXHDL_Run_Arch(
-        hdl,
-        "And",
-        (AXHDL_Signal[]){ in,AXHDL_Signal_Null() },
-        (AXHDL_Signal[]){ out,AXHDL_Signal_Null() }
-    );
-    printf("[Sim]: Out: %x\n",((AXHDL_U8*)out.data)[0]);
-
-    AXHDL_Signal_Free(&out);
-    AXHDL_Signal_Free(&in);
-}
+#include "/home/codeleaded/System/Static/Library/ChipBench.h"
 
 int main(){
+    ChipBench cb = ChipBench_New("./assets/digits");
+    ChipBench_Load(&cb,"./bin/std");
+
     AXHDL hdl = AXHDL_New("./bin");
-    
-    AXHDL_Push_Signal(&hdl,AXHDL_Signal_New("u1","clk",(AXHDL_U8[]){ 0x01U },1U,AXHDL_SIGNAL_DIR_OUT));
-    AXHDL_Push_Signal(&hdl,AXHDL_Signal_New("u1","reset",(AXHDL_U8[]){ 0x01U },1U,AXHDL_SIGNAL_DIR_OUT));
-    
     AXHDL_Script(&hdl,"./code/Main.axhdl");
     AXHDL_Print(&hdl);
 
-    AXHDL_Run_Sim(&hdl,1ULL,Fn_Runner);
+    AXHDL_Build(&hdl,&cb.cs);
+    
+    ChipBench_Print(&cb);
+    ChipBench_Start(&cb);
+    
+    while(cb.w.Running){
+        if(cb.w.Strokes[ALX_MOUSE_L].PRESSED){
+            Chip_Signal ins[] = { cb.w.MouseX,cb.w.MouseY };
+            Chip_Signal outs[] = { 0x0ULL };
+            
+            ChipSet_Exe(
+                &cb.cs,
+                "MAIN",
+                ins,
+                outs
+            );
+        }
+
+        if(!cb.w.Focus) Thread_Sleep_M(40);
+        else            Thread_Sleep_M(2);
+    }
+
+    ChipBench_Free(&cb);
     AXHDL_Free(&hdl);
     return 0;
 }
