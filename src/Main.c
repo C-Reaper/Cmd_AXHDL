7a271f66594550533545d1a92813e80723ac6734
codeleaded <gnu.c.reaper@gmail.com>
2026-03-15 21:58:00 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -4,6 +4,7 @@
 int main(){
     ChipBench cb = ChipBench_New("./assets/digits");
     ChipBench_Load(&cb,"./bench/std");
+    ChipBench_Load(&cb,"./bench/seg7");
 
     AXHDL hdl = AXHDL_New("./bin");
     AXHDL_Script(&hdl,"./code/Main.axhdl");
@@ -13,24 +14,33 @@ int main(){
 
     AXHDL_Build(&hdl,&cb.cs);
     
+    Chip_Def* cd = ChipSet_Find(&cb.cs,"MAIN");
+    ChipBench_AddGUI(&cb,"SEG7",10U,0U,  (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,2) });
+    ChipBench_AddGUI(&cb,"SEG7",10U,250U,(Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,3) });
+
     ChipBench_Print(&cb);
     ChipBench_Start(&cb);
     
     while(cb.w.Running){
-        if(cb.w.Strokes[ALX_MOUSE_L].PRESSED){
-            Chip_Signal ins[] = { cb.w.MouseX,cb.w.MouseY };
-            Chip_Signal outs[] = { 0x0ULL };
-            
-            ChipSet_Exe(
-                &cb.cs,
-                "MAIN",
-                ins,
-                outs
-            );
-        }
+        //if(cb.w.Strokes[ALX_MOUSE_L].PRESSED){}
 
-        if(!cb.w.Focus) Thread_Sleep_M(40);
-        else            Thread_Sleep_M(2);
+        Chip_Signal ins[] = { 0x0ULL,0x0ULL };
+        Chip_Signal outs[] = { 0x0ULL,0x0ULL };
+
+        if(cb.w.Strokes[ALX_KEY_W].DOWN){ ins[0] = 0x1ULL; }
+        if(cb.w.Strokes[ALX_KEY_S].DOWN){ ins[1] = 0x1ULL; }
+        
+        ChipSet_Exe(
+            &cb.cs,
+            "MAIN",
+            ins,
+            outs
+        );
+
+        printf("Output: %llu,%llu\n",outs[0],outs[1]);
+
+        if(!cb.w.Focus) Thread_Sleep_M(1000);
+        else            Thread_Sleep_M(1000);
     }
 
     ChipBench_Free(&cb);
