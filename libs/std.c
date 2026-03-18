037198228171fd31088c33640a23179ea46a862a
codeleaded <gnu.c.reaper@gmail.com>
2026-03-18 04:49:57 +0100

    updated

--- libs/std.c
+++ libs/std.c
@@ -26,4 +26,12 @@ void Ex_Packer(ExternFunctionMap* Extern_Functions,Vector* funcs,AXHDL* hdl){ //
         AXHDL_Comp_New("unsigned","c",1,AXHDL_SIGNAL_DIR_OUT),
         AXHDL_Comp_Null()
     })));
+
+    AXHDL_Arch_Map_Add(&hdl->archs,AXHDL_Arch_Make("MUX",AXHDL_Comp_Map_Make((AXHDL_Comp[]){
+        AXHDL_Comp_New("unsigned","a",1,AXHDL_SIGNAL_DIR_IN),
+        AXHDL_Comp_New("unsigned","b",1,AXHDL_SIGNAL_DIR_IN),
+        AXHDL_Comp_New("unsigned","s",1,AXHDL_SIGNAL_DIR_IN),
+        AXHDL_Comp_New("unsigned","c",1,AXHDL_SIGNAL_DIR_OUT),
+        AXHDL_Comp_Null()
+    })));
 }
