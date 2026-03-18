ac75cc2fb784155ec7a2535e1c524a3c8ff950b9
codeleaded <gnu.c.reaper@gmail.com>
2026-03-18 13:59:40 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -10,37 +10,39 @@ int main(){
     AXHDL_Script(&hdl,"./code/Main.axhdl");
 
     AXHDL_Compile(&hdl);
-    AXHDL_Print(&hdl);
+    //AXHDL_Print(&hdl);
 
     AXHDL_Build(&hdl,&cb.cs);
-    
-    Chip_Def* cd = ChipSet_Find(&cb.cs,"MAIN");
-    ChipBench_AddGUI(&cb,"SEG7",10U,0U,  (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,2) });
-    ChipBench_AddGUI(&cb,"SEG7",10U,250U,(Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,3) });
 
-    ChipBench_Print(&cb);
-    ChipBench_Start(&cb);
-    
-    while(cb.w.Running){
-        //if(cb.w.Strokes[ALX_MOUSE_L].PRESSED){}
+    if(!hdl.xc){
+        Chip_Def* cd = ChipSet_Find(&cb.cs,"MAIN");
+        ChipBench_AddGUI(&cb,"SEG7",10U,0U,  (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,2) });
+        ChipBench_AddGUI(&cb,"SEG7",10U,250U,(Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,3) });
 
-        Chip_Signal ins[] = { 0x0ULL,0x0ULL };
-        Chip_Signal outs[] = { 0x0ULL,0x0ULL };
-
-        if(cb.w.Strokes[ALX_KEY_W].DOWN){ ins[0] = 0x1ULL; }
-        if(cb.w.Strokes[ALX_KEY_S].DOWN){ ins[1] = 0x1ULL; }
+        ChipBench_Print(&cb);
+        ChipBench_Start(&cb);
         
-        ChipSet_Exe(
-            &cb.cs,
-            "MAIN",
-            ins,
-            outs
-        );
+        while(cb.w.Running){
+            //if(cb.w.Strokes[ALX_MOUSE_L].PRESSED){}
 
-        printf("Output: %llu,%llu\n",outs[0],outs[1]);
+            Chip_Signal ins[] = { 0x0ULL,0x0ULL };
+            Chip_Signal outs[] = { 0x0ULL,0x0ULL };
 
-        if(!cb.w.Focus) Thread_Sleep_M(1000);
-        else            Thread_Sleep_M(1000);
+            if(cb.w.Strokes[ALX_KEY_W].DOWN){ ins[0] = 0x1ULL; }
+            if(cb.w.Strokes[ALX_KEY_S].DOWN){ ins[1] = 0x1ULL; }
+
+            ChipSet_Exe(
+                &cb.cs,
+                "MAIN",
+                ins,
+                outs
+            );
+
+            printf("Output: %llu,%llu\n",outs[0],outs[1]);
+
+            if(!cb.w.Focus) Thread_Sleep_M(1000);
+            else            Thread_Sleep_M(1000);
+        }
     }
 
     ChipBench_Free(&cb);
