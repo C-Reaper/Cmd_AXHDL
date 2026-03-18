2a32b68a3ffae069afc7cdbaa9ddacf5390dfe74
codeleaded <gnu.c.reaper@gmail.com>
2026-03-18 23:02:03 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -20,9 +20,9 @@ int main(){
         ChipBench_AddGUI(&cb,"SEG7",10U,0U,     (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s0")) });
         ChipBench_AddGUI(&cb,"SEG7",10U,250U,   (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s1")) });
         ChipBench_AddGUI(&cb,"SEG7",10U,500U,   (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s2")) });
-        ChipBench_AddGUI(&cb,"SEG7",500U,0U,    (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s3")) });
-        ChipBench_AddGUI(&cb,"SEG7",500U,250U,  (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s4")) });
-        ChipBench_AddGUI(&cb,"SEG7",500U,500U,  (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s5")) });
+        // ChipBench_AddGUI(&cb,"SEG7",500U,0U,    (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s3")) });
+        // ChipBench_AddGUI(&cb,"SEG7",500U,250U,  (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s4")) });
+        // ChipBench_AddGUI(&cb,"SEG7",500U,500U,  (Chip_Impl*[]){ (Chip_Impl*)Vector_Get(&cd->chips,AXHDL_Chip_Map_FindI(&aa->chips,"s5")) });
 
         //ChipBench_Print(&cb);
         ChipBench_Start(&cb);
@@ -30,11 +30,17 @@ int main(){
         while(cb.w.Running){
             //if(cb.w.Strokes[ALX_MOUSE_L].PRESSED){}
 
-            Chip_Signal ins[] = { 0x0ULL,0x0ULL };
-            Chip_Signal outs[] = { 0x0ULL,0x0ULL,0x0ULL,0x0ULL,0x0ULL,0x0ULL };
+            //Chip_Signal ins[] = { 0x0ULL,0x0ULL };
+            //Chip_Signal outs[] = { 0x0ULL,0x0ULL,0x0ULL,0x0ULL,0x0ULL,0x0ULL };
 
-            if(cb.w.Strokes[ALX_KEY_W].DOWN){ ins[0] = 0x1ULL; }
-            if(cb.w.Strokes[ALX_KEY_S].DOWN){ ins[1] = 0x1ULL; }
+            Chip_Signal ins[] = { 0x0ULL,0x0ULL };
+            Chip_Signal outs[] = { 0x0ULL };
+
+            //if(cb.w.Strokes[ALX_KEY_W].DOWN){ ins[0] = 0x1ULL; }
+            //if(cb.w.Strokes[ALX_KEY_S].DOWN){ ins[1] = 0x1ULL; }
+
+            ins[0] = cb.w.MouseX;
+            ins[1] = cb.w.MouseY;
 
             ChipSet_Exe(
                 &cb.cs,
@@ -43,7 +49,8 @@ int main(){
                 outs
             );
 
-            printf("Output: %llu,%llu,%llu,%llu,%llu,%llu\n",outs[0],outs[1],outs[2],outs[3],outs[4],outs[5]);
+            //printf("Output: %llu,%llu,%llu,%llu,%llu,%llu\n",outs[0],outs[1],outs[2],outs[3],outs[4],outs[5]);
+            printf("Output: %llu\n",outs[0]);
 
             if(!cb.w.Focus) Thread_Sleep_M(1000);
             else            Thread_Sleep_M(1000);
